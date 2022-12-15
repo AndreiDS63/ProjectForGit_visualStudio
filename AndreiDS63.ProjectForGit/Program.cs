94b855c3d9ab3e52281fe05216b23dcdb27c3142
AndreiDS63 <dolzhikov_as@mail.ru>
2022-12-15 10:37:13 +0400

    Add project files.

---
+++ AndreiDS63.ProjectForGit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndreiDS63.ProjectForGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя!");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}");
+            Console.ReadLine();
+        }
+    }
+}
